06682c52ac9464edd051641a2907f46683ce3433
DwyaneChou <dwyanechou@qq.com>
2019-06-05 00:43:51 +0800

    Trim code

--- src/tend_mod.f90
+++ src/tend_mod.f90
@@ -8,6 +8,9 @@ MODULE tend_mod
     real, dimension(:,:,:), allocatable :: u
     real, dimension(:,:,:), allocatable :: v
     real, dimension(:,:,:), allocatable :: phi
+    
+    real, dimension(:,:,:), allocatable :: zonal_wind
+    real, dimension(:,:,:), allocatable :: meridional_wind
   end type tend_field
   
   type(tend_field), dimension(:), allocatable :: tend ! allocated by n time points, which is used by temporal integration schemes
@@ -23,6 +26,9 @@ MODULE tend_mod
       allocate(tend(iT)%u  (ids:ide,jds:jde,Nf))
       allocate(tend(iT)%v  (ids:ide,jds:jde,Nf))
       allocate(tend(iT)%phi(ids:ide,jds:jde,Nf))
+      
+      allocate(tend(iT)%zonal_wind     (ids:ide,jds:jde,ifs:ife))
+      allocate(tend(iT)%meridional_wind(ids:ide,jds:jde,ifs:ife))
     enddo
     
   end subroutine initTend
