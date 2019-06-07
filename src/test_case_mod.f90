547ea2341a02a3dc6b8cca941495a46f7851edb2
DwyaneChou <dwyanechou@qq.com>
2019-06-07 22:55:32 +0800

    Add Rossby-Haurwitz wave test case

--- src/test_case_mod.f90
+++ src/test_case_mod.f90
@@ -11,6 +11,7 @@ module test_case_mod
     
   subroutine initTestCase
     if(case_num == 2) call case2(stat(0))
+    if(case_num == 6) call case6(stat(0))
     
     print*,''
     print*,'max/min value of u   : ',maxval(stat(0)%u  ),minval(stat(0)%u  )
@@ -53,64 +54,85 @@ module test_case_mod
   subroutine case6(stat)
     type(stat_field), intent(inout) :: stat
     real,parameter              :: omg  = 7.848d-6         ! angular velocity of RH wave
-    real,parameter              :: R    = 4d0              ! wave number of RH wave
-    real,parameter              :: h0   = 8000.d0          ! wave number of RH wave
+    real,parameter              :: R    = 4              ! wave number of RH wave
+    real,parameter              :: h0   = 8000.          ! wave number of RH wave
     
-    real,dimension(0:nx1,0:ny1) :: u1,u2,u3                ! working array
-    real,dimension(0:nx1,0:ny1) :: AA1,Ac,A21,A22,A23,Ah   ! working array
-    real,dimension(0:nx1,0:ny1) :: Bc,BB1,BB2,Bh           ! working array
-    real,dimension(0:nx1,0:ny1) :: CC,CC1,CC2,Ch           ! working array
-    real,dimension(0:nx1,0:ny1) :: coslat                  ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: u,v,phi                 ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: u1,u2,u3                ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: AA1,Ac,A21,A22,A23,Ah   ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: Bc,BB1,BB2,Bh           ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: CC,CC1,CC2,Ch           ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: sinlat                  ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: coslat                  ! working array
+    real,dimension(ips:ipe,jps:jpe,ifs:ife) :: longitude               ! working array
     
-    integer                     :: i,j                     ! working variable
+    integer                                 :: i,j,iPatch              ! working variable
     
-    coslat       = cos_lat
-    coslat(:,1 ) = 0.d0
-    coslat(:,ny) = 0.d0
+    sinlat    = sin(mesh%latP)
+    coslat    = cos(mesh%latP)
+    longitude = mesh%lonP
     
-    do j=1,ny
-        do i=1,nx
-            u1(i,j) = coslat(i,j)
-            u2(i,j) = R*coslat(i,j)**(R-1)*sin_lat(i,j)**2*dcos(R*longitude(i,j))
-            u3(i,j) = coslat(i,j)**(R+1)*dcos(R*longitude(i,j))
-            u (i,j) = a*omg*(u1(i,j)+u2(i,j)-u3(i,j))
-            
-            v (i,j) = -a*omg*R*coslat(i,j)**(R-1)*sin_lat(i,j)*dsin(R*longitude(i,j))
-            
-            AA1 (i,j) = omg*0.5d0*(2.d0*omg0+omg)*coslat(i,j)**2
-            Ac  (i,j) = 0.25*omg**2
-            A21 (i,j) = (R+1.d0)*coslat(i,j)**(2.d0*R+2.d0)
-            A22 (i,j) = (2.d0*R**2-R-2.d0)*coslat(i,j)**(2.d0*R)
-            A23 (i,j) = 2.d0*R**2*coslat(i,j)**(2.d0*R-2)
-            Ah  (i,j) = AA1(i,j)+Ac(i,j)*(A21(i,j)+A22(i,j)-A23(i,j))
-            
-            Bc  (i,j) = 2.*(omg0+omg)*omg/((R+1)*(R+2))*coslat(i,j)**R
-            BB1 (i,j) = R**2+2.d0*R+2.d0
-            BB2 (i,j) = (R+1.d0)**2.*coslat(i,j)**2
-            Bh  (i,j) = Bc(i,j)*(BB1(i,j)-BB2(i,j))
-            
-            CC  (i,j) = 0.25*omg**2*coslat(i,j)**(2.d0*R)
-            CC1 (i,j) = (R+1.d0)*coslat(i,j)**2;
-            CC2 (i,j) = R+2.d0
-            Ch  (i,j) = CC(i,j)*(CC1(i,j)-CC2(i,j))
-            
-            wh (i,j) = g*h0+a**2*(Ah(i,j) + Bh(i,j)*dcos(R*longitude(i,j)) + Ch(i,j)*dcos(2.0*R*longitude(i,j)))
+    do iPatch = ifs, ife
+      do j = jps, jpe
+        do i = ips, ipe
+          u1(i,j,iPatch) = coslat(i,j,iPatch)
+          u2(i,j,iPatch) = R*coslat(i,j,iPatch)**(R-1)*sinlat(i,j,iPatch)**2*cos(R*longitude(i,j,iPatch))
+          u3(i,j,iPatch) = coslat(i,j,iPatch)**(R+1)*cos(R*longitude(i,j,iPatch))
+          u (i,j,iPatch) = radius*omg*(u1(i,j,iPatch)+u2(i,j,iPatch)-u3(i,j,iPatch))
+          
+          v (i,j,iPatch) = -radius*omg*R*coslat(i,j,iPatch)**(R-1)*sinlat(i,j,iPatch)*sin(R*longitude(i,j,iPatch))
+          
+          AA1 (i,j,iPatch) = omg*0.5*(2.*Omega+omg)*coslat(i,j,iPatch)**2
+          Ac  (i,j,iPatch) = 0.25*omg**2
+          A21 (i,j,iPatch) = (R+1.)*coslat(i,j,iPatch)**(2.*R+2.)
+          A22 (i,j,iPatch) = (2.*R**2-R-2.)*coslat(i,j,iPatch)**(2.*R)
+          A23 (i,j,iPatch) = 2.*R**2*coslat(i,j,iPatch)**(2.*R-2)
+          Ah  (i,j,iPatch) = AA1(i,j,iPatch)+Ac(i,j,iPatch)*(A21(i,j,iPatch)+A22(i,j,iPatch)-A23(i,j,iPatch))
+          
+          Bc  (i,j,iPatch) = 2.*(Omega+omg)*omg/((R+1)*(R+2))*coslat(i,j,iPatch)**R
+          BB1 (i,j,iPatch) = R**2+2.*R+2.
+          BB2 (i,j,iPatch) = (R+1.)**2.*coslat(i,j,iPatch)**2.
+          Bh  (i,j,iPatch) = Bc(i,j,iPatch)*(BB1(i,j,iPatch)-BB2(i,j,iPatch))
+          
+          CC  (i,j,iPatch) = 0.25*omg**2*coslat(i,j,iPatch)**(2.*R)
+          CC1 (i,j,iPatch) = (R+1.)*coslat(i,j,iPatch)**2;
+          CC2 (i,j,iPatch) = R+2.
+          Ch  (i,j,iPatch) = CC(i,j,iPatch)*(CC1(i,j,iPatch)-CC2(i,j,iPatch))
+          
+          phi (i,j,iPatch) = gravity*h0+radius**2*(Ah(i,j,iPatch) + Bh(i,j,iPatch)*cos(R*longitude(i,j,iPatch)) + Ch(i,j,iPatch)*cos(2.0*R*longitude(i,j,iPatch)))
         enddo
+      enddo
     enddo
     
-    u (0  ,:) = u (nx,:)
-    v (0  ,:) = v (nx,:)
-    wh(0  ,:) = wh(nx,:)
-    u (nx1,:) = u (1 ,:)
-    v (nx1,:) = v (1 ,:)
-    wh(nx1,:) = wh(1 ,:)
+    stat%zonal_wind      = u
+    stat%meridional_wind = v
+    stat%phi             = phi
+    stat%phiG            = stat%phi * mesh%sqrtG
     
-    u (:,0  ) = 0
-    u (:,ny1) = 0
-    v (:,0  ) = 0
-    v (:,ny1) = 0
-    wh(:,0  ) = 0
-    wh(:,ny1) = 0
+    do iPatch = ifs, ife
+      do j = jps, jpe
+        do i = ips, ipe
+          call covProjSphere2Plane    (stat%u      (i,j,iPatch), stat%v      (i,j,iPatch), stat%zonal_wind(i,j,iPatch), stat%meridional_wind(i,j,iPatch), mesh%matrixIA(:,:,i,j,iPatch), mesh%matrixG(:,:,i,j,iPatch))
+          call contravProjSphere2Plane(stat%contraU(i,j,iPatch), stat%contraV(i,j,iPatch), stat%zonal_wind(i,j,iPatch), stat%meridional_wind(i,j,iPatch), mesh%matrixIA(:,:,i,j,iPatch))
+        enddo
+      enddo
+    enddo
+    
+    mesh%phi_s = 0.
+    
+    !u  (0  ,:) = u (nx,:)
+    !v  (0  ,:) = v (nx,:)
+    !phi(0  ,:) = phi(nx,:)
+    !u  (nx1,:) = u (1 ,:)
+    !v  (nx1,:) = v (1 ,:)
+    !phi(nx1,:) = phi(1 ,:)
+    !
+    !u  (:,0  ) = 0
+    !u  (:,ny1) = 0
+    !v  (:,0  ) = 0
+    !v  (:,ny1) = 0
+    !phi(:,0  ) = 0
+    !phi(:,ny1) = 0
   end subroutine case6
   
 end module test_case_mod
