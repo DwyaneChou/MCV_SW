9784a2e202e0aa4c88b201df2555ed39842f4a58
DwyaneChou <dwyanechou@qq.com>
2019-06-19 02:49:52 +0800

    Modify vorticity algorithm

--- src/spatial_operators_mod.f90
+++ src/spatial_operators_mod.f90
@@ -17,16 +17,24 @@ MODULE spatial_operators_mod
       real flux_y(ids:ide,jds:jde,ifs:ife)
       real div_x (ids:ide,jds:jde,ifs:ife)
       real div_y (ids:ide,jds:jde,ifs:ife)
+      real dvdx  (ids:ide,jds:jde,ifs:ife)
+      real dudy  (ids:ide,jds:jde,ifs:ife)
       
       real vorticity(ids:ide,jds:jde,ifs:ife)
       
-      real lambda_x(ips:ipe) ! eigenvalue along x direction
-      real lambda_y(jps:jpe) ! eigenvalue along y direction
+      real lambda_x  (ips:ipe) ! eigenvalue along x direction
+      real lambda_y  (jps:jpe) ! eigenvalue along y direction
+      real upstream_x(ips:ipe) ! eigenvalue along x direction
+      real upstream_y(jps:jpe) ! eigenvalue along y direction
       
-      real ux   (ips:ipe) ! u array along x direction
-      real vy   (jps:jpe) ! v array along y direction
-      real phiGx(ips:ipe) ! phiG array along x direction
-      real phiGy(jps:jpe) ! phiG array along y direction
+      real ux      (ips:ipe) ! u array along x direction
+      real uy      (jps:jpe) ! u array along y direction
+      real vx      (ips:ipe) ! v array along x direction
+      real vy      (jps:jpe) ! v array along y direction
+      real contraUx(ips:ipe) ! contravariant u array along x direction
+      real contraVy(jps:jpe) ! contravariant v array along x direction
+      real phiGx   (ips:ipe) ! phiG array along x direction
+      real phiGy   (jps:jpe) ! phiG array along y direction
       
       real K (ips:ipe,jps:jpe,ifs:ife) ! K = 0.5 * (contraU*coU + contraV*coV)
       real E (ips:ipe,jps:jpe,ifs:ife) ! E = phi + phi_s + K
@@ -50,46 +58,60 @@ MODULE spatial_operators_mod
       !$OMP PARALLEL SECTIONS PRIVATE(iPatch)
       !$OMP SECTION
         ! calculate tend in x direction
-        !$OMP PARALLEL DO PRIVATE(i,j,Ex,ux,phiGux,PhiGx,lambda_x)
+        !$OMP PARALLEL DO PRIVATE(i,j,Ex,ux,vx,contraUx,phiGux,PhiGx,lambda_x,upstream_x)
         do iPatch = ifs, ife
           do j = jds, jde
-            Ex    = E        (:,j,iPatch)
-            ux    = stat%u   (:,j,iPatch)
-            phiGux= phiGu    (:,j,iPatch)
-            phiGx = stat%phiG(:,j,iPatch)
+            Ex       = E           (:,j,iPatch)
+            ux       = stat%u      (:,j,iPatch)
+            vx       = stat%v      (:,j,iPatch)
+            contraUx = stat%contraU(:,j,iPatch)
+            phiGux   = phiGu       (:,j,iPatch)
+            phiGx    = stat%phiG   (:,j,iPatch)
             
             do i = ips, ipe
               lambda_x(i) = eigenvalue_x(stat%contraU(i,j,iPatch),stat%phi(i,j,iPatch),mesh%matrixIG(:,:,i,j,iPatch))
             enddo
             
-            call calc_tendP(flux_x(:,j,iPatch),Ex    ,ux   ,lambda_x)
-            call calc_tendP(div_x (:,j,iPatch),phiGux,phiGx,lambda_x)
+            where(contraUx==0)contraUx=1.
+            upstream_x = contraUx / abs(contraUx)
+            
+            call calc_tendP(flux_x(:,j,iPatch),Ex    ,ux   ,lambda_x  )
+            call calc_tendP(div_x (:,j,iPatch),phiGux,phiGx,lambda_x  )
+            call calc_tendP(dvdx  (:,j,iPatch),vx    ,vx   ,upstream_x)
           enddo
         enddo
         !$OMP END PARALLEL DO
       
       !$OMP SECTION
         ! calculate tend in y direction
-        !$OMP PARALLEL DO PRIVATE(i,j,Ey,vy,phiGvy,PhiGy,lambda_y)
+        !$OMP PARALLEL DO PRIVATE(i,j,Ey,uy,vy,contraVy,phiGvy,PhiGy,lambda_y,upstream_y)
         do iPatch = ifs, ife
           do i = ids, ide
-            Ey     = E        (i,:,iPatch)
-            vy     = stat%v   (i,:,iPatch)
-            phiGvy = phiGv    (i,:,iPatch)
-            phiGy  = stat%phiG(i,:,iPatch)
+            Ey       = E           (i,:,iPatch)
+            uy       = stat%u      (i,:,iPatch)
+            vy       = stat%v      (i,:,iPatch)
+            contraVy = stat%contraV(i,:,iPatch)
+            phiGvy   = phiGv       (i,:,iPatch)
+            phiGy    = stat%phiG   (i,:,iPatch)
             
             do j = jps, jpe
               lambda_y(j) = eigenvalue_y(stat%contraV(i,j,iPatch),stat%phi(i,j,iPatch),mesh%matrixIG(:,:,i,j,iPatch))
             enddo
             
-            call calc_tendP(flux_y(i,:,iPatch),Ey    ,vy   ,lambda_y)
-            call calc_tendP(div_y (i,:,iPatch),phiGvy,phiGy,lambda_y)
+            where(contraVy==0)contraVy=1.
+            upstream_y = contraVy / abs(contraVy)
+            
+            call calc_tendP(flux_y(i,:,iPatch),Ey    ,vy   ,lambda_y  )
+            call calc_tendP(div_y (i,:,iPatch),phiGvy,phiGy,lambda_y  )
+            call calc_tendP(dudy  (i,:,iPatch),uy    ,uy   ,upstream_y)
           enddo
         enddo
         !$OMP END PARALLEL DO
       !$OMP END PARALLEL SECTIONS
       
-      call calc_vorticity(vorticity,stat%u,stat%v)
+      !call calc_vorticity(vorticity,stat%u,stat%v)
+      
+      vorticity = (dvdx - dudy) / mesh%sqrtG(ids:ide,jds:jde,ifs:ife)
       
       tend%phiG = -div_x - div_y
       tend%u    = -flux_x + mesh%sqrtG(ids:ide,jds:jde,:) * (vorticity + mesh%f(ids:ide,jds:jde,:)) * stat%contraV(ids:ide,jds:jde,:)
